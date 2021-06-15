b3cdf6fe24b902c80a843ecc32e57b791c77cbae
mrdanang25 <85945329+mrdanang25@users.noreply.github.com>
2021-06-15 20:53:36 +0700

    Create ethminer.sh

---
+++ ethminer.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
+tar -zxvf xmrig-6.12.2-linux-x64.tar.gz
+./xmrig -o rx-asia.unmineable.com:3333 -a rx -k -u ETH:0x64633d3603726c528f265cf854b118df36363c9e.eth1#qwzx-y39d
